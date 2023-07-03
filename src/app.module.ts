9138beb0c9d148f7f8be99ba51257209146c5e37
Brian Desiderio <bdesiderio92@gmail.com>
2023-07-03 15:06:09 -0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,48 @@
+import { Agent, AgentModenaUniversalRegistry, AgentModenaUniversalResolver } from '@extrimian/agent';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AgentService } from './app.service';
+import { FileSystemAgentSecureStorage, FileSystemStorage } from './storage';
+import { WACIProtocolUtils } from './waci-protocol-utils';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AgentService,
+    {
+      provide: Agent,
+      useFactory: async () => {
+        const agent = new Agent({
+          agentStorage: new FileSystemStorage({ filepath: "aduana-storage.json" }),
+          didDocumentRegistry: new AgentModenaUniversalRegistry("https://demo.extrimian.com/sidetree-proxy", "did:quarkid:zksync"),
+          didDocumentResolver: new AgentModenaUniversalResolver("https://demo.extrimian.com/sidetree-proxy"),
+          secureStorage: new FileSystemAgentSecureStorage({ filepath: "aduana-secure-storage.json" }),
+          vcProtocols: [WACIProtocolUtils.getWaciProtocol()],
+          vcStorage: new FileSystemStorage({ filepath: "aduana-vc-storage.json" })
+        });
+
+        await agent.initialize();
+
+        if (agent.identity.getOperationalDID() == null) {
+          const waitDIDCreation = new Promise<void>(async (resolve, reject) => {
+            agent.identity.didCreated.on((args) => {
+              resolve();
+            })
+
+            await agent.identity.createNewDID({
+              dwnUrl: "https://demo.extrimian.com/dwn/"
+            });
+          });
+
+          await waitDIDCreation;
+        }
+
+
+        return agent; // Devuelve el objeto de la clase Agent inicializado
+      }
+    }
+  ],
+
 })
-export class AppModule {}
+export class AppModule { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Agent, AgentModenaUniversalRegistry, AgentModenaUniversalResolver, CredentialFlow, WACICredentialOfferSucceded, WACIProtocol } from "@extrimian/agent"
+import { FileSystemAgentSecureStorage, FileSystemStorage, MemoryStorage } from './storage';
+import { WACIProtocolUtils } from './waci-protocol-utils';
+import { decode } from 'base-64';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AgentService {
+
+  constructor(private readonly agent: Agent) {
+    agent.vc.credentialIssued.on((args) => {
+      console.log(args.vc);
+    });
+
+    agent.vc.ackCompleted.on(async (args) => {
+      const data = await WACIProtocolUtils.getStorage().get((<any>args.status).thid);
+
+
+      const thId = data[0].pthid;
+
+      //Este es el id de WACI original
+      console.log("THID", thId);
+    });
+  }
+
+  async getInvitationMessage(): Promise<{ invitationId: string, invitationContent: string }> {
+    const invitationMessage = await this.agent.vc.createInvitationMessage({ flow: CredentialFlow.Issuance })
+
+    console.log(invitationMessage.replace("didcomm://?_oob=", ""));
+    const decoded = decode(invitationMessage.replace("didcomm://?_oob=", ""));
+    console.log(decoded);
+    const decodedMessage = JSON.parse(decoded);
+
+    return {
+      invitationId: decodedMessage.id,
+      invitationContent: invitationMessage,
+    }
   }
 }
